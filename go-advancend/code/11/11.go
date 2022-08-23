8bb0525995092289f3c58917a8de80b2df368c93
xiongxinwei <3293172751nss@gmail.com>
2022-08-23 11:13:14 +0800

    file

--- go-advancend/code/11/11.go
+++ go-advancend/code/11/11.go
@@ -2,7 +2,7 @@
  * @Author: xiongxinwei 3293172751nss@gmail.com
  * @Date: 2022-08-22 17:12:36
  * @LastEditors: xiongxinwei 3293172751nss@gmail.com
- * @LastEditTime: 2022-08-22 17:20:50
+ * @LastEditTime: 2022-08-23 11:11:19
  * @FilePath: \code\11\11.go
  * @Description: 值、引用、指针
  */
@@ -12,21 +12,9 @@ import (
 	"fmt"
 )
 
-func changeInt(i int) {
-	i += 1
-}
-
-func changeSlice(s []string) {
-	for i, v := range s {
-
-	}
-}
-
 func main() {
 	fmt.Println("hello word")
 	var i int
 	i = 1
-	changeInt(i)
 	fmt.Println(i)
-
 }
