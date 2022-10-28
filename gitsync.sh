775774153124a2542960b62958a5ecfd09bca866
xiongxinwei <3293172751nss@gmail.com>
2022-10-28 23:33:36 +0800

    feat(bash-script)： xiongxinwei update
    Signed-off-by: xiongxinwei <3293172751nss@gmail.com>

--- gitsync.sh
+++ gitsync.sh
@@ -19,11 +19,16 @@
 
     # windows下面没有bash，所以需要使用git-bash来执行脚本
 
-    # wget -c -d -O agit.sh https://sm.nsddd.top/uploads/2022/10/26/2iCzooCq_gitsync.sh && sh agit.sh && chmod 777 agit.sh
+    # 脚本代码在线浏览
+    # http://xxw.nsddd.top/code?p=%2Fgitsync.sh&id=AN16H9
+
+    # Linux一键下载
+    # wget -c -d -O gitsync.sh https://sm.nsddd.top/uploads/2022/10/27/Y0iHb6ix_gitsync.sh?attname=gitsync.sh && echo "gitsync.sh" >> .gitignore && sh gitsync.sh && chmod 777 gitsync.sh \
+
  
  ### 
-echo "程序将会自动帮您同步⚡ 请稍等...(The program will automatically help you synchronize ⚡ Please wait...)"
-echo "脚本地址在线浏览：https://sm.nsddd.top/uploads/2022/10/26/2iCzooCq_gitsync.sh"
+
+
 is_change=$(git status -s)  # 判断是否有文件改动
 
 # 当前分支
@@ -38,8 +43,88 @@ else
 fi
 
 if [ 0 -lt ${#is_change} ]; then  # 有文件改动
+    echo "监测到程序改动，程序将会自动帮您同步⚡ 请稍等...(The program will automatically help you synchronize ⚡ Please wait...)"
+    echo " "
+    echo "Myblog: http://nsddd.top"
+    echo
+    echo "脚本地址在线浏览🧷：https://sm.nsddd.top/uploads/2022/10/26/2iCzooCq_gitsync.sh"
+
+    is_change=$(git status -s)  # 判断是否有文件改动
+
+    # 输出git信息
+    git status && git remote -v && git branch -a
+
+echo "是否选择提交类型(默认选择yes)？(Y/N)"
+    # 输入  
+    read -p "=> " choice
+    # 判断
+    if [ $choice == "n" ] || [ $choice == "N" ]; then
+        echo "您选择了不输入提交类型，这将会省略commit的<类型>参数"
+    else
+        echo "⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️"
+        echo "1: feat：新功能（feature）"
+        echo "2: fix：修补bug"
+        echo "3: docs：文档（documentation）"
+        echo "4: style： 格式（不影响代码运行的变动）"
+        echo "5: refactor：重构（即不是新增功能，也不是修改bug的代码变动）"
+        echo "6: test：增加测试"
+        echo "7: chore：构建过程或辅助工具的变动"
+        echo "8: revert：回滚"
+        echo "9: perf：性能优化"
+        echo "⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️"
+
+        feat="feat：新功能（feature）"
+        fix="fix：修补bug"
+        docs="docs：文档（documentation）"
+        style="style： 格式（不影响代码运行的变动）"
+        refactor="refactor：重构（即不是新增功能，也不是修改bug的代码变动）"
+        test="test：增加测试"
+        chore="chore：构建过程或辅助工具的变动"
+        revert="revert：回滚"
+        perf="perf：性能优化"
+
+        echo "请输入提交类型(前面的序号)："
+        read -p "=> " type
+
+        echo "请输入对应的说明"
+        read -p "=> " desc
+        if [ $type == "1" ]; then
+            type=feat
+            type2=$feat
+        elif [ $type == "2" ]; then
+            type=fix
+            type2=$fix
+        elif [ $type == "3" ]; then
+            type=docs
+            type2=$docs
+        elif [ $type == "4" ]; then
+            type=style
+            type2=$style
+        elif [ $type == "5" ]; then
+            type=refactor
+            type2=$refactor
+        elif [ $type == "6" ]; then
+            type=test
+            type2=$test
+        elif [ $type == "7" ]; then
+            type=chore
+            type2=$chore
+        elif [ $type == "8" ]; then
+            type=revert
+            type2=$revert
+        elif [ $type == "9" ]; then
+            type=perf
+            type2=$perf
+        else
+            echo "输入错误，将会使用默认提交类型"
+            type=feat
+            type=$feat
+        fi
+        echo "<$type>您输入的提交类型为 💡$type2  对应的说明为 💡$desc"
+    fi
+
     git add .
-    git commit -s -m "$guser"  # -s 用于签名, -m 用于备注
+    git commit -s -m "$type($desc)： $guser"  # -s 用于签名, -m 用于备注
     # pull
     result=$(git pull origin $branch)
     tmp=$(echo $result | grep "fix conflicts")
@@ -47,8 +132,15 @@ if [ 0 -lt ${#is_change} ]; then  # 有文件改动
     then
         echo "(ノ=Д=)ノ┻━┻ 合并冲突, 请手动解决后提交"
     else
-        # 推送
+        # 推送并且保证推送成功
         git push origin $branch
+    fi
+
+    # 判断推动是否成功
+    if [ $? -eq 0 ]; then  
+        echo "⚠️ 推送成功"
+    else
+        echo "⚠️ 推送失败"
     fi
     
 else  # 没有文件改动
@@ -60,3 +152,6 @@ else  # 没有文件改动
       echo "(ノ=Д=)ノ┻━┻ 合并冲突, 请手动解决后提交"
     fi
 fi
+
+# 打印一条log日志
+git log -q -1 --use-mailmap --source   --mailmap   # -q 用于简化输出, -1 用于显示最近1条, --use-mailmap 用于显示作者名, --source 用于显示提交类型, --mailmap 用于显示作者邮箱
