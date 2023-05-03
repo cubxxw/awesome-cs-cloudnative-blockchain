88a9caf07f5dee47f15344deeaf163cafa4658eb
Xinwei Xiong <3293172751nss@gmail.com>
2023-05-03 10:03:36 +0800

    docs: cs and cryptology file fix readme
    Signed-off-by: Xinwei Xiong <3293172751nss@gmail.com>

--- .gitsync.sh
+++ .gitsync.sh
@@ -11,13 +11,13 @@
     # 3. git push 
     # 4. git pull
 
-    # Linux下可以直接使用crontab -e命令来编辑定时任务，Windows下可以使用计划任务来实现定时任务的功能。
+    # Linux下可以直接使用crontab -e命令来编辑定时任务,Windows下可以使用计划任务来实现定时任务的功能。
     # crontab -e
     # 0 0 * * * /bin/bash /home/xxx/gitsync.sh
 
-    # 不推荐使用pull，因为pull会把本地的修改覆盖掉，如果本地有修改，应该先commit，然后再pull
+    # 不推荐使用pull,因为pull会把本地的修改覆盖掉,如果本地有修改,应该先commit,然后再pull
 
-    # windows下面没有bash，所以需要使用git-bash来执行脚本
+    # windows下面没有bash,所以需要使用git-bash来执行脚本
 
     # 脚本代码在线浏览
     # http://xxw.nsddd.top/code?p=%2Fgitsync.sh&id=AN16H9
@@ -43,11 +43,11 @@ else
 fi
 
 if [ 0 -lt ${#is_change} ]; then  # 有文件改动
-    echo "监测到程序改动，程序将会自动帮您同步⚡ 请稍等...(The program will automatically help you synchronize ⚡ Please wait...)"
+    echo "监测到程序改动,程序将会自动帮您同步⚡ 请稍等...(The program will automatically help you synchronize ⚡ Please wait...)"
     echo " "
     echo "Myblog: http://nsddd.top"
     echo
-    echo "脚本地址在线浏览🧷：https://sm.nsddd.top/uploads/2022/10/26/2iCzooCq_gitsync.sh"
+    echo "脚本地址在线浏览🧷: https://sm.nsddd.top/uploads/2022/10/26/2iCzooCq_gitsync.sh"
 
     is_change=$(git status -s)  # 判断是否有文件改动
 
@@ -59,18 +59,18 @@ echo "是否选择提交类型(默认选择yes)？(Y/N)"
     read -p "=> " choice
     # 判断
     if [ $choice == "n" ] || [ $choice == "N" ]; then
-        echo "您选择了不输入提交类型，这将会省略commit的<类型>参数"
+        echo "您选择了不输入提交类型,这将会省略commit的<类型>参数"
     else
         echo "⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️"
-        echo "1: feat：新功能（feature）"
-        echo "2: fix：修补bug"
-        echo "3: docs：文档（documentation）"
-        echo "4: style： 格式（不影响代码运行的变动）"
-        echo "5: refactor：重构（即不是新增功能，也不是修改bug的代码变动）"
-        echo "6: test：增加测试"
-        echo "7: chore：构建过程或辅助工具的变动"
-        echo "8: revert：回滚"
-        echo "9: perf：性能优化"
+        echo "1: feat: 新功能(feature)"
+        echo "2: fix: 修补bug"
+        echo "3: docs: 文档(documentation)"
+        echo "4: style:  格式(不影响代码运行的变动)"
+        echo "5: refactor: 重构(即不是新增功能,也不是修改bug的代码变动)"
+        echo "6: test: 增加测试"
+        echo "7: chore: 构建过程或辅助工具的变动"
+        echo "8: revert: 回滚"
+        echo "9: perf: 性能优化"
         echo "⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️"
 
         feat="feat"
@@ -83,7 +83,7 @@ echo "是否选择提交类型(默认选择yes)？(Y/N)"
         revert="revert"
         perf="perf"
 
-        echo "请输入提交类型(前面的序号)："
+        echo "请输入提交类型(前面的序号): "
         read -p "=> " type
 
         echo "请输入对应的说明"
@@ -116,7 +116,7 @@ echo "是否选择提交类型(默认选择yes)？(Y/N)"
             type=perf
             type2=$perf
         else
-            echo "输入错误，将会使用默认提交类型"
+            echo "输入错误,将会使用默认提交类型"
             type=feat
             type=$feat
         fi
